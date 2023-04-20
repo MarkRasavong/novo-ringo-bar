2eb5f6670b6f24e62c36768d8ee66da441c492b4
Mark Rasavong <rasavong.mark@gmail.com>
2023-04-20 19:43:43 +0200

    Edit: theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = {
+  ringoRed: '#ff090c',
+  ringoWhite: '#ffffff',
+  ringoBeige: '#f7f3ed',
+  ringoGray: '#707070',
+  ringoGreen: '#2ECC71',
+  ringoBlue: '#3498DB',
+  ringoPurple: '#9B59B6',
+};
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -10,7 +21,7 @@ module.exports = {
       sans: ['Poppins', 'sans-serif'],
       condensed: ['Roboto Condensed', 'sans-serif'],
     },
-    colors: {},
+    colors: { ...colors, transparent: 'transparent', current: 'currentColor' },
   },
   plugins: [],
 };
