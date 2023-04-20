2061f583b191ccb66836c48f9eb41e244fffaaa1
Mark Rasavong <rasavong.mark@gmail.com>
2023-04-20 21:35:45 +0200

    Add: Layout & grow main to even header and footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
+import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
+    <Layout>
       <Component {...pageProps} />
-      <Footer />
-    </>
+    </Layout>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body style={{ minHeight: '390vh' }}>
+      <body>
         <Main />
         <NextScript />
       </body>
