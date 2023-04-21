84ec89bedde89d5785f17cbe835f00d409b2889b
Mark Rasavong <rasavong.mark@gmail.com>
2023-04-21 20:05:32 +0200

    Edit: GaleriaSection recieves images, alt, and swipable galery

--- src/components/GaleriaSection.tsx
+++ src/components/GaleriaSection.tsx
@@ -1,58 +1,51 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.min.css';
+import { urlFor } from '@/lib/client';
 import { SanityGaleriaSchema } from '@/sanity/schemas/galeria';
 import Image from 'next/image';
-import { useState } from 'react';
-
+import { Navigation, Pagination } from 'swiper';
 interface GaleriaSectionProps {
   galeria: SanityGaleriaSchema;
 }
 
 const GaleriaSection: React.FC<GaleriaSectionProps> = ({ galeria }) => {
-  console.log(galeria);
-
-  const [isLoaded, setIsLoaded] = useState([false, false, false]);
-
-  const testImg = '/pizza1.jpg';
-
-  const handleLoad = (idx: number) => {
-    setIsLoaded(prev => {
-      const newState = [...prev];
-      newState[idx] = true;
-      return newState;
-    });
-  };
-
+  const { images } = galeria;
   return (
     <section className="bg-gray-100 py-16" id="galeria">
       <div className="container mx-auto">
         <h2 className="text-4xl md:text-6xl font-bold text-center mb-8 text-ringoRed">
           Galería
         </h2>
-
-        <Image
-          src={testImg}
-          alt={`Pizza123`}
-          width={100}
-          height={100}
-          className={`block rounded-md`}
-        />
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {[1, 2, 3].map((itm, idx) => (
-            <div className="relative" id={String(idx)} key={idx}>
-              {!isLoaded[idx] && (
-                <div className="animate-pulse">
-                  <div className="bg-gray-300 rounded-md h-64 w-full"></div>
-                </div>
-              )}
+        <Swiper
+          style={{ padding: '0 50px' }}
+          spaceBetween={50}
+          modules={[Navigation, Pagination]}
+          navigation={true}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {images.map(itm => (
+            <SwiperSlide key={itm._key}>
               <Image
-                src={testImg}
-                alt={`Pizza ${idx}`}
-                fill
-                onLoad={() => handleLoad(idx)}
-                className={`${isLoaded[idx] ? 'block' : 'hidden'} rounded-md`}
+                height={400}
+                width={400}
+                src={urlFor(itm.asset._ref).height(400).width(400).toString()}
+                alt={itm.description}
+                className={`block rounded-md`}
               />
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
       </div>
     </section>
   );
