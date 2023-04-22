b1361f44eb6d7c310191089f895ac7d6e1942b7f
Mark Rasavong <rasavong.mark@gmail.com>
2023-04-22 20:24:58 +0200

    Edit: nanocss dep

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    cssnano: {
+      preset: 'default',
+      discardComments: {
+        removeAll: true,
+      },
+      generateScopedName: '[hash:base64:5]',
+    },
   },
-}
+};
