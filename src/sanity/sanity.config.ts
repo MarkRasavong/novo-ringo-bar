24cbb150799de5b2bfbf3fc6ae3015a4e5435eb1
Mark Rasavong <rasavong.mark@gmail.com>
2023-05-03 17:36:39 +0200

    Add: language filter to studio.sanity.io

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {languageFilter} from '@sanity/language-filter'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,17 @@ export default defineConfig({
   projectId: 'tf5jy7b0',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    languageFilter({
+      supportedLanguages: [
+        {id: 'es', title: 'Español'},
+        {id: 'en', title: 'English'},
+      ],
+      defaultLanguages: ['es'],
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
